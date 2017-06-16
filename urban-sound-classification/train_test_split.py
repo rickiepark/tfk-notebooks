c2d93840efaa40e4810466b083d556b1630174ee
Haesun Park <haesunrpark@gmail.com>
2017-06-16 17:50:51 +0900

    modify for GroupShuffleSplit

--- urban-sound-classification/train_test_split.py
+++ urban-sound-classification/train_test_split.py
@@ -1,11 +1,24 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GroupShuffleSplit
 
 sound_data = np.load('urban_sound.npz')
 X_data = sound_data['X']
 y_data = sound_data['y']
+groups = sound_data['groups']
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
+print(groups[groups > 0])
 
-np.savez('urban_sound_train', X=X_train, y=y_train)
-np.savez('urban_sound_test', X=X_test, y=y_test)
+gss = GroupShuffleSplit(n_splits=1, test_size=0.2)
+for train_idx, test_idx in gss.split(X_data, y_data, groups=groups):
+    X_train = X_data[train_idx]
+    y_train = y_data[train_idx]
+    groups_train = groups[train_idx]
+
+    X_test = X_data[test_idx]
+    y_test = y_data[test_idx]
+    groups_test = groups[test_idx]
+    
+    print(X_train.shape, X_test.shape)
+    
+np.savez('urban_sound_train', X=X_train, y=y_train, groups=groups_train)
+np.savez('urban_sound_test', X=X_test, y=y_test, groups=groups_test)
